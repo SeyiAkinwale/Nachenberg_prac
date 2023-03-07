3f77c1b2efaa391fc86f1233acd575079ee2425e
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-03-07 20:13:22 +0000

    2 hundreds

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
+using namespace std;
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
+int main() 
+{
+  int i1 = 100, i2;
+  int* pi;
+
+  pi = &i1;
+
+  cout<< i1 << *pi;
+
+  *pi = 10;
+//  cout << i1;
+
+  i2 = *pi;
+//  cout << i2;
 }
