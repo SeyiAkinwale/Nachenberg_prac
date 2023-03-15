3f7e49369e5f322e1c7d6e303ff8e2dff66cedd3
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-03-15 10:15:17 +0000

    some output view

--- .ccls-cache/@home@runner@Nachenbergprac/main.cpp
+++ .ccls-cache/@home@runner@Nachenbergprac/main.cpp
@@ -5,7 +5,11 @@ void mystery (int* &ptr, int& a, int& b);
 
 int main ()
 {
+  int x = 100;
+  int* ptr1 = &x;
 
+  cout << "x = " << x << endl;
+  cout << "ptr1 = " << ptr1 << endl;
   return 0;
 }
 
