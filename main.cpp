b4aaec7b26bc62cbeb2f8e48cb415d0ee7069176
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-03-15 21:06:56 +0000

    removed comments

--- main.cpp
+++ main.cpp
@@ -32,29 +32,7 @@ int main ()
 
   *(studentInfo[0]->intPointer)=23;
     cout<< "*(studentInfo[0])->intPointer " << *(studentInfo[0]->intPointer) <<endl;
-  // cout << "x = " << x << endl;
-  // cout << "ptr1 = " << ptr1 << endl;
 
-  // int** ptr2 = &ptr1;
-  // cout << "ptr2 = " << ptr2 << endl;
-
-  // **ptr2=200;
-  // cout << "x = " << x << endl;
-
-  // int*** ptr3 = &ptr2;
-  // ***ptr3 = 300;
-  // cout << "x = " << x << endl;
-
-  //int x1 = 10, x2 = 20, x3 = 30, x4 = 40;
-  // int* p1 = &x1;
-  // int* p2 = &x2;
-  // int* p3 = &x3;
-  // int* p4 = &x4;
-  // int* quartet[]= {p1, p2, p3, p4};
-  //int* quartet[]= {&x1, &x2, &x3, &x4};
-  // cout << "*p1= " << p1 << " *p2 " << p2 <<endl;
-  // cout << "*p3= " << p3 << " *p4 " << p4 <<endl;
-  //for (int* i: quartet) {cout<<i<<endl;}
   return 0;
   
 }
