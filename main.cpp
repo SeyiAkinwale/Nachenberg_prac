cd3547155404f2bb433586f54ce4a0e9433e2606
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-03-15 11:39:05 +0000

    class with member pointer

--- main.cpp
+++ main.cpp
@@ -1,14 +1,23 @@
 using namespace std;
 #include <iostream>
 
-void mystery (int* &ptr, int& a, int& b);
+class Student {
+  public:
+    string name = "blank";
+    int* intPointer = nullptr;
+};
 
 
 int main ()
 {
-  // int x = 100;
+  int x1 = 10, x2 = 20, x3 = 30, x4 = 40;
   // int* ptr1 = &x;
 
+  Student Allan;
+  Allan.intPointer = &x1;
+
+  cout <<"*(Allan.intPointer) " <<*(Allan->intPointer);
+  
   // cout << "x = " << x << endl;
   // cout << "ptr1 = " << ptr1 << endl;
 
@@ -22,14 +31,16 @@ int main ()
   // ***ptr3 = 300;
   // cout << "x = " << x << endl;
 
-  int x1 = 10, x2 = 20, x3 = 30, x4 = 40;
-  int* p1 = &x1;
-  int* p2 = &x2;
-  int* p3 = &x3;
-  int* p4 = &x4;
-  int* quartet[]= {p1, p2, p3, p4};
-  cout << "*p1= " << p1 << " *p2 " << p2 <<endl;
-  cout << "*p3= " << p3 << " *p4 " << p4 <<endl;
+  //int x1 = 10, x2 = 20, x3 = 30, x4 = 40;
+  // int* p1 = &x1;
+  // int* p2 = &x2;
+  // int* p3 = &x3;
+  // int* p4 = &x4;
+  // int* quartet[]= {p1, p2, p3, p4};
+  //int* quartet[]= {&x1, &x2, &x3, &x4};
+  // cout << "*p1= " << p1 << " *p2 " << p2 <<endl;
+  // cout << "*p3= " << p3 << " *p4 " << p4 <<endl;
+  //for (int* i: quartet) {cout<<i<<endl;}
   return 0;
   
 }
