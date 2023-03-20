41694ac19bfcb11b763986b39634c7226267555f
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-03-20 20:17:22 +0000

    Dereferencing  corrected

--- main.cpp
+++ main.cpp
@@ -4,16 +4,23 @@ using namespace std;
 
 bool match(const char str1[], const char str2[])
 {
-  while (str1 != 0 && str2 !=0) //zero bytes at ends
+  while (*str1 != 0 && *str2 !=0) //zero bytes at ends
   {
-    if(str1 != str2) //compare corresponding characters
-      return false;
-    str1++;
+    if(*str1 != *str2)   //compare corresponding characters
+     return false;
+    str1++;            //advance to the next character
+    str2++;
+
   }
+  return *str1 == *str2; //both ended at same time?
 }
 
 int main ()
 {
+  char a[10] = "pointless";
+  char b[10] = "pointless";
 
+  if (match(a,b)) cout<< "They're the same!\n";
+  
   return 0;
 }
