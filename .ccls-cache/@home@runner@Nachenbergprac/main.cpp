0d3c3ff226327f70689aa2fa275a6151b47b96f4
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-03-20 19:11:10 +0000

    seems like square root is neutralized

--- .ccls-cache/@home@runner@Nachenbergprac/main.cpp
+++ .ccls-cache/@home@runner@Nachenbergprac/main.cpp
@@ -2,15 +2,16 @@ using namespace std;
 #include <iostream>
 #include <cmath>
 
-void hypotenuse(double leg1, double leg2, double* &resultPtr)
+void hypotenuse(double leg1, double leg2, double* resultPtr)
 {
-  *resultPtr = sqrt(leg1*leg1 + leg2*leg2);
+  *resultPtr = (leg1*leg1 + leg2*leg2);
 }
 
 int main ()
 {
   double* p;
-  double  x1 = 0;  p= &x1;
+  double x1 = 0;
+  p = &x1;
   
   //hypotenuse(1.5, 2.0, p);
   cout << "The hypotenuse is " << *p << endl;
