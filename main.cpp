2ee0e8a4cd39b443d7a225e91fab97f96e9ee86c
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-03-20 18:48:22 +0000

    dangling pointer?

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@ using namespace std;
 #include <iostream>
 #include <cmath>
 
-void hypotenuse(double leg1, double leg2, double* resultPtr)
+void hypotenuse(double leg1, double leg2, double* &resultPtr)
 {
   *resultPtr = sqrt(leg1*leg1 + leg2*leg2);
 }
@@ -10,7 +10,9 @@ void hypotenuse(double leg1, double leg2, double* resultPtr)
 int main ()
 {
   double* p;
-  hypotenuse(1.5, 2.0, p);
+  double  x1 = 0; double* p= &x1;
+  
+  //hypotenuse(1.5, 2.0, p);
   cout << "The hypotenuse is " << *p << endl;
   return 0;
 }
