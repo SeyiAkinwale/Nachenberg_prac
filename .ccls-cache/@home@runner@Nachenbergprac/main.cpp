006c7dbdfbe76571911f629e366c825835c87ba3
SeyiAkinwale1 <72584469+SeyiAkinwale@users.noreply.github.com>
2023-03-20 18:02:17 +0000

    mystery func pointers

--- .ccls-cache/@home@runner@Nachenbergprac/main.cpp
+++ .ccls-cache/@home@runner@Nachenbergprac/main.cpp
@@ -1,25 +1,34 @@
 using namespace std;
 #include <iostream>
 
-class Node {
-  public:
-    int** x = nullptr;
-    int m = 0;
-};
-
+void mystery (int* &ptr, int a, int** b);
 
 int main ()
 {
-  int   x0=10;
-  int*  y0= &x0;
-  int** z0= &y0;
-  Node aNode;
-  aNode.x=&x0;
-  cout<<aNode.m<<endl;
-  cout<<"aNode.x = "<< aNode.x;
-  int x1 = 10, x2 = 20, x3 = 30, x4 = 40;  
+  int num1 = 15, num2 = 9, num3 = 10;
+  int *p = &num1;
 
+  mystery(p, num2, &p);
+
+  cout << "num1: " << num1 << "num2: " <<num2;
+  cout << "num3: " << num3 << endl;
+
+  if (p == &num1) cout << " num1";
+  else if (p == &num2) cout << " num2";
+  else if (p == &num3) cout << " num3";
   return 0;
-  
+}
+
+void mystery (int* &ptr, int a, int** b)
+{
+  a++;
+  if (a> **b)
+    ptr = &a;
+  else
+  {
+    ptr = *b;
+    *b = &a;
+  }
+  (*ptr) +=10;
 }
 
